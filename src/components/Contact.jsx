0545c60c9a2a033d347df028dff7cca6558d2b7e
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-15 18:30:21 +0545

    Add contact section style and content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,34 @@
 function Contact() {
   return (
-    <div className="h-screen" id="contact">
-      <h1>Contact</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt sit
-        explicabo veritatis beatae labore reprehenderit voluptate accusamus
-        corrupti porro ea quis numquam quasi veniam aut, eum obcaecati nihil
-        dolore rerum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nostrum est similique recusandae officia fuga. A qui, voluptate saepe
-        sequi eos vitae, possimus ipsa facere tempora quas quo dolorem debitis
-        alias!
-      </p>
-    </div>
+    <section className="my-24 flex flex-col" id="contact">
+      <h1 className="m-10 self-center text-5xl font-medium">Contact me</h1>
+      <div className="flex gap-10 rounded-2xl bg-slate-900 px-20 py-10">
+        <div className="flex w-6/12 flex-col items-center">
+          <span className="mb-2 block w-fit rounded-sm bg-slate-700 px-2 py-1 text-sm font-semibold text-cyan-400">
+            Linkedin
+          </span>
+          <a
+            href="https://www.linkedin.com/in/adhikarijenish/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="border-b w-fit"
+          >
+            @adhikarijenish
+          </a>
+        </div>
+        <div className="flex w-6/12 flex-col items-center">
+          <span className="mb-2 block w-fit rounded-sm bg-slate-700 px-2 py-1 text-sm font-semibold text-green-400">
+            Personal Mail
+          </span>
+          <a
+            href="mailto:jenishadhikari2060@gmail.com"
+            className="border-b"
+          >
+            jenishadhikari2060@gmail.com
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
 
