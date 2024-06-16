e39a67cc86948a7c01bbd0c457ba99422f312066
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-16 18:49:55 +0545

    Update file

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,13 @@
 function Experience() {
   return (
-    <div className="h-screen" id="experience">
-      <h1>Experience</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt sit
-        explicabo veritatis beatae labore reprehenderit voluptate accusamus
-        corrupti porro ea quis numquam quasi veniam aut, eum obcaecati nihil
-        dolore rerum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nostrum est similique recusandae officia fuga. A qui, voluptate saepe
-        sequi eos vitae, possimus ipsa facere tempora quas quo dolorem debitis
-        alias!
-      </p>
+    <div
+      className="h-fit w-full text-white md:flex md:flex-col md:pt-10"
+      id="experience"
+    >
+      <h1 className="text-center text-3xl font-medium md:text-4xl">
+        Experience
+      </h1>
+      <p className="p-12 text-center text-lg"><em>Looking For Internship</em></p>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,11 @@
 function Projects() {
   return (
-    <div className="h-screen" id="projects">
-      <h1>Projects</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt sit
-        explicabo veritatis beatae labore reprehenderit voluptate accusamus
-        corrupti porro ea quis numquam quasi veniam aut, eum obcaecati nihil
-        dolore rerum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nostrum est similique recusandae officia fuga. A qui, voluptate saepe
-        sequi eos vitae, possimus ipsa facere tempora quas quo dolorem debitis
-        alias!
-      </p>
+    <div
+      className="h-fit w-full pt-8 text-white md:flex md:flex-col md:pt-10"
+      id="projects"
+    >
+      <h1 className="text-center text-3xl font-medium md:text-4xl">Projects</h1>
+      <p className="p-12 text-center text-lg"><em>In Progress ...</em></p>
     </div>
   );
 }
