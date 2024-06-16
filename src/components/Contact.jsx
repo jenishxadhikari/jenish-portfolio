adc50dfad9f9a8fc807b134a2080f0f58bcdc062
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-16 18:50:32 +0545

    Configure Contact.jsx to be responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,14 @@
 function Contact() {
   return (
-    <section className="my-24 flex flex-col" id="contact">
-      <h1 className="m-10 self-center text-5xl font-medium">Contact me</h1>
-      <div className="flex gap-10 rounded-2xl bg-slate-900 px-20 py-10">
-        <div className="flex w-6/12 flex-col items-center">
+    <section
+      className="flex h-fit w-full flex-col items-center py-12 text-white md:pt-10"
+      id="contact"
+    >
+      <h1 className="text-center text-3xl font-medium md:text-4xl">
+        Contact me
+      </h1>
+      <div className="bg-slate-900 mt-5 flex w-9/12 flex-col items-center justify-center gap-10 rounded-2xl px-20 py-10 md:w-fit md:flex-row">
+        <div className="flex w-fit flex-col items-center md:w-6/12">
           <span className="mb-2 block w-fit rounded-sm bg-slate-700 px-2 py-1 text-sm font-semibold text-cyan-400">
             Linkedin
           </span>
@@ -11,18 +16,18 @@ function Contact() {
             href="https://www.linkedin.com/in/adhikarijenish/"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-b w-fit"
+            className="w-fit border-b text-sm"
           >
             @adhikarijenish
           </a>
         </div>
-        <div className="flex w-6/12 flex-col items-center">
+        <div className="flex w-fit flex-col items-center md:w-6/12">
           <span className="mb-2 block w-fit rounded-sm bg-slate-700 px-2 py-1 text-sm font-semibold text-green-400">
             Personal Mail
           </span>
           <a
             href="mailto:jenishadhikari2060@gmail.com"
-            className="border-b"
+            className="border-b text-sm"
           >
             jenishadhikari2060@gmail.com
           </a>
