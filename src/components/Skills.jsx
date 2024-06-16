ce44ed27d5fb8529ef084744427bf42f44331cf7
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-16 18:49:12 +0545

    Fix & configure Skill.jsx to be responsive

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,13 +2,19 @@ import { Card } from "../components";
 
 function Skills() {
   return (
-    <section className="h-screen w-9/12 pt-10" id="skills">
-      <h1 className="text-center text-4xl font-medium">Skills</h1>
-      <div className="mt-10 flex gap-10">
-        <Card title={'Languages'} />
-        <Card title={'Framework'} />
-        <Card title={'Database'} />
-        <Card title={'Tools'} />
+    <section className="flex h-screen flex-col items-center text-white">
+      <h1 className="pt-16 text-center text-4xl font-medium" id="skills">
+        Skills
+      </h1>
+      <div className="mt-10 flex w-10/12 flex-col gap-6 md:flex-row md:justify-center">
+        <div className="flex gap-1 md:gap-6">
+          <Card title={"Languages"} />
+          <Card title={"Framework"} />
+        </div>
+        <div className="flex gap-1 md:gap-6">
+          <Card title={"Database"} />
+          <Card title={"Tools"} />
+        </div>
       </div>
     </section>
   );
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,9 +1,55 @@
 import PropTypes from "prop-types";
+import { IoLogoJavascript } from "react-icons/io5";
+import { TbBrandCpp, TbBrandRedux, TbBrandTailwind } from "react-icons/tb";
+import {
+  FaReact,
+  FaDatabase,
+  FaGitAlt,
+  FaHtml5,
+  FaCss3Alt,
+} from "react-icons/fa";
+import { BiLogoMongodb } from "react-icons/bi";
+import { IoLogoFirebase } from "react-icons/io5";
 
 function Card({ title }) {
+  const categories = {
+    Languages: [
+      { icon: <IoLogoJavascript />, text: "JavaScript" },
+      { icon: <TbBrandCpp />, text: "C++" },
+      { icon: <FaHtml5 />, text: "HTML" },
+      { icon: <FaCss3Alt />, text: "CSS" },
+    ],
+    Framework: [
+      { icon: <FaReact />, text: "React" },
+      { icon: <TbBrandTailwind />, text: "TailwindCSS" },
+      { icon: <TbBrandRedux />, text: "Redux-Toolkit" },
+    ],
+    Database: [
+      { icon: <FaDatabase />, text: "MySQL" },
+      { icon: <BiLogoMongodb />, text: "MongoDB" },
+    ],
+    Tools: [
+      { icon: <FaGitAlt />, text: "Git" },
+      { icon: <IoLogoFirebase />, text: "Firebase" },
+    ],
+  };
+
+  const items = categories[title] || [];
+
   return (
-    <div className="mt-10 h-[300px] w-[300px] rounded-md bg-gradient-to-b from-slate-900 to-slate-800 p-6 text-center">
-      <h2 className="mb-2 text-lg font-medium">{title}</h2>
+    <div className= "bg-slate-900 h-fit w-6/12 rounded-md p-6 text-center md:h-[300px] md:w-6/12 md:px-10 shadow-lg shadow-navHover">
+      <h2 className="mb-1 md:mb-4 text-lg font-medium">{title}</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className="flex list-none items-center p-1 text-start text-sm"
+          >
+            <span className="mr-2">{item.icon}</span>
+            {item.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
