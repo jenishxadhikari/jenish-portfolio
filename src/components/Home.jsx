23b35c808b3dfa59c41e3568ef0960d4ae8e8b00
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-16 18:47:43 +0545

    Configure Home to be responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,10 @@ import { IoIosArrowDown } from "react-icons/io";
 function Home() {
   return (
     <article
-      className="flex h-screen w-full flex-col items-center pt-36"
+      className="flex h-screen flex-col items-center justify-end text-white md:justify-center"
       id="home"
     >
-      <h1 className="self-center text-5xl font-medium">
+      <h1 className="mb-4 text-4xl font-medium md:mb-6 md:mt-32 md:text-5xl">
         <TypewriterComponent
           options={{
             strings: ["Jenish Adhikari"],
@@ -20,17 +20,17 @@ function Home() {
           }}
         />
       </h1>
-      <span className="my-5 text-4xl">🇳🇵</span>
-      <h2 className="mb-5 text-2xl text-gray-400">
+      <span className="mb-4 text-4xl">🇳🇵</span>
+      <h2 className="mb-6 px-4 text-center text-xl text-gray-400 md:text-2xl">
         Computer Science + Full Stack Web Development
       </h2>
-      <div className="mb-6 flex items-center gap-5 text-2xl">
+      <div className="mb-10 flex items-center gap-5 text-2xl md:mb-5">
         <Socials />
       </div>
-      <button className="text-md rounded-full border border-solid border-sky-400 px-5 py-2 font-semibold text-cyan-400 hover:bg-cyan-400/10">
-        Resume <MdArrowOutward className="inline text-xl" />
+      <button className="rounded-full border border-solid border-sky-400 px-3 py-2 text-xs font-semibold text-cyan-400 hover:bg-cyan-400/10 md:text-sm">
+        Resume <MdArrowOutward className="md:text-md inline text-sm" />
       </button>
-      <div className="mt-10">
+      <div className="my-24 md:my-10">
         <IoIosArrowDown className="duration-400 animate-ping text-4xl text-cyan-400/70 delay-100" />
         <IoIosArrowDown className="duration-400 animate-ping text-4xl text-cyan-400/70 delay-300" />
         <IoIosArrowDown className="duration-400 animate-ping text-4xl text-cyan-400/70 delay-700" />
